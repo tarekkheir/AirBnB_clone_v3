75ece5e580fd232f94878bbd0f15f7f006d443cd
toyugo <toyugo>
2021-05-10 20:12:27 +0000

    add password condition

--- models/base_model.py
+++ models/base_model.py
@@ -67,7 +67,7 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if getenv('HBNB_TYPE_STORAGE') == 'db':
+        if getenv('HBNB_TYPE_STORAGE') == 'db' and "password" in new_dict:
             del new_dict["password"]
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
