a8d4a951c54a734c85101e47a6203a7a1af5b5f4
toyugo <toyugo>
2021-05-10 11:02:05 +0000

    __init__ fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Init """
 from flask import Blueprint
+
+
+app_views = Blueprint('test', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
@@ -8,4 +11,3 @@ from api.v1.views.places import *
 from api.v1.views.cities import *
 from api.v1.views.users import *
 
-app_views = Blueprint('test', __name__, url_prefix='/api/v1')
